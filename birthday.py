f267fe21e23e832b0c7cd026c28f2b0e318d8c51
Harris <Amelia.Harris@rjuhsd.int>
2022-01-25 12:51:17 -0800

    Asks for b day :)

--- birthday.py
+++ birthday.py
@@ -1,1 +1,9 @@
+import datetime
+
 print("Welcome.... :)")
+month = input("Enter your birth month: ")
+day = input("Enter your birth day: ")
+year = input("Enter your birth year ")
+
+print("YOUR BIRTHDAY ISSSSSS : ", month, day, year)
+
