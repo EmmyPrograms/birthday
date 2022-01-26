145928dd17014a7fc60b587f44f8a4c2e2beacfc
Crockett <bcrockett@RJUHSD.INT>
2022-01-26 12:19:08 -0800

    Imported datetime and got todays date as a str

--- birthday.py
+++ birthday.py
@@ -1,10 +1,23 @@
-import datetime
+from datetime import date
+import time
 
 print("Welcome.... :)")
+name = input("What is your name.")
+
 month = input("Enter your birth month: ")
 day = input("Enter your birth day: ")
 year = input("Enter your birth year ")
 
 print("YOUR BIRTHDAY ISSSSSS : ", month, day, year)
-print("Happy birthday")
-print("Happy birthday")
+for i in range(3):
+    print("Happy birthday. To you.")
+    time.sleep(1)
+
+print("Happy birthday dear.", name)
+
+td = str(date.today())
+td_arr = td.split("-")
+cy = td_arr[0]
+cm = td_arr[1]
+cd = td_arr[2]
+print(type(cy), type(cm), type(cd))
